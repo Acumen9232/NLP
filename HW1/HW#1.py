1513ba57fc03487f18afea6d94141d19ce635b08
Acumen <Acumen9232@gmail.com>
2021-07-21 20:19:09 +0800

    clear

--- HW1/HW#1.py
+++ HW1/HW#1.py
@@ -4,31 +4,76 @@
 # In[ ]:
 
 
-# encoding=utf-8
-import string
+import re
 import jieba
 import jieba.analyse
+import matplotlib.pyplot as plt
+from collections import Counter
+from pyecharts.charts import Bar,Line
 
-f=open("hw1-dataset.txt", "r" ,encoding = "utf8")
-lines=f.readlines()
 
-for i in range(0,lines.__len__(),1):
+def DrawB(keyList,valueList):
     
-    list=[]
-    for word in lines[i].split():
-        word=word.strip(string.whitespace)
-        list.append(word);
-    for line in list:
+        
+        bar=Bar()
+        bar.add_xaxis(keyList)
+        bar.add_yaxis("關鍵字統計",valueList)
+        bar.render("Freq.html")
+        
+def DrawL(keyList,valueList)->Line:
+    
+        line=Line()
+        line.add_xaxis(keyList)
+        line.add_yaxis("TF/IDF統計",valueList)
+        line.render("TFIDF.html")
+    
+def Jieba_TFIDF():
+    
+    sentence=open("hw1-dataset.txt","rb").read()
+    keywords=jieba.analyse.extract_tags(sentence,topK=100,withWeight=True,allowPOS=())
+    keyList=[n for n in range(1,100)]
+    valueList=[]
+    print('\nTF/IDF結果:')
+    i=1
+    for item in keywords:
+        print(i,item[0],item[1])
+        i+=1
+        valueList.append(item[1])
+    DrawL(keyList,valueList)
+        
+def Jieba_Freq():
+    
+    cut_words=""
+    for line in open('hw1-dataset.txt',encoding='utf-8'):
+        line.strip('\n')
+        line=re.sub("[\'\: \·\—\\.\，\。\“ \”\n\u3000\？\、\'*\',\']", "", line)
         seg_list=jieba.cut(line,cut_all=False)
-        print("Default Mode: " + "/ ".join(seg_list))
+        cut_words+=(" ".join(seg_list))
+    all_words=cut_words.split()
+    c=Counter()
+    total=0
+    for x in all_words:
+        if len(x)>1 and x!= '\r\n':
+            c[x]+=1
+            total+=1        
+    print('\n頻率結果:')
+    dict={}
+    
+    i=1
+    for(k,v) in c.most_common(100):
+        dict[k]=v
+        print("%d.%s:%f"%(i,k,v/total))
+        i+=1
+    keyList=[n for n in range(1,100)]
+    valueList=[]
+    for k,v in dict.items():
+        valueList.append(v/total)
+    DrawB(keyList,valueList)
 
-f.close()
+if __name__ == "__main__":
 
-"""f=open("hw1-dataset.txt", "r" ,encoding = "utf8")
-for lines in f.readlines():
-    seg_list=jieba.cut(lines,cut_all=False)
-#    print("Default Mode: " + "/ ".join(seg_list))
-f.close()"""
+    Jieba_Freq()
+    Jieba_TFIDF()
 
 
 # In[ ]:
